0a9a72cc79d6299378169a8a3e1a19a2575f01e4
Andre <75341645+andrembarreto@users.noreply.github.com>
2023-09-02 19:56:03 -0300

    Declared public methods implementation

--- applicationfile.cpp
+++ applicationfile.cpp
@@ -5,3 +5,23 @@ ApplicationFile::ApplicationFile(QObject *parent)
 {
 
 }
+
+void ApplicationFile::block() {
+
+}
+
+void ApplicationFile::unblock(QString pass) {
+
+}
+
+void ApplicationFile::setUnblockPass(QString pass) {
+
+}
+
+void ApplicationFile::setBlockTime(int time) {
+
+}
+
+bool ApplicationFile::isBlocked() {
+    return _blocked;
+}
